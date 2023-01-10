1cb1fe9a948e4a07ee7b79ae2a8f1db72a4fe8f3
niruc100 <49512133+niruc100@users.noreply.github.com>
2023-01-11 00:33:02 +0100

    update due to more data

--- src/main/python/train_model.py
+++ src/main/python/train_model.py
@@ -7,16 +7,16 @@ from sklearn.linear_model import PassiveAggressiveClassifier
 if __name__ == "__main__":
     
     print("Load training data...")
-    aug_claims = pd.read_csv("../../../data/augmented_claims.csv")
+    aug_claims = pd.read_csv("../../../data/preprocessed_claims.csv")
     X = aug_claims["claim"].values
     y = aug_claims["truth_rating"].values
-    print(f"Loaded augmented data with shapes: {X.shape}, {y.shape}")
+    print(f"Loaded data with shapes: {X.shape}, {y.shape}")
     
     print("Train model")
     vec = TfidfVectorizer()
     vec.fit(X)
     X_ = vec.transform(X)
-    clf = PassiveAggressiveClassifier(random_state=417, n_jobs=-1, C=0.0001)
+    clf = PassiveAggressiveClassifier(random_state=417, n_jobs=-1, C=0.01)
     clf.fit(X_, y)
     
     print("Save trained model...")
